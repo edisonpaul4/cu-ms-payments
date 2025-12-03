8a3c7da925b0098b74c15f8330f036a208466f84
Paul Mosquera <edpmosqu@pichincha.com>
2025-12-02 19:34:53 -0500

    corrijo tag en k8s.yaml

--- app.py
+++ app.py
@@ -9,10 +9,29 @@ PORT = 3000
 
 class HolaMundoHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html; charset=utf-8')
-        self.end_headers()
-        self.wfile.write(b'<h1>Hola Mundo</h1>')
+        if self.path == '/startup':
+            print(f"se llamo al endpoints /startup")
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html; charset=utf-8')
+            self.end_headers()
+            self.wfile.write(b'OK')
+        elif self.path == '/liveness':
+            print(f"se llamo al endpoints /liveness")
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html; charset=utf-8')
+            self.end_headers()
+            self.wfile.write(b'OK')
+        elif self.path == '/readiness':
+            print(f"se llamo al endpoints /readiness")
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html; charset=utf-8')
+            self.end_headers()
+            self.wfile.write(b'OK')
+        else:
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html; charset=utf-8')
+            self.end_headers()
+            self.wfile.write(b'<h1>Hola Mundo</h1>')
     
     def log_message(self, format, *args):
         print(f"{self.address_string()} - {format%args}")
